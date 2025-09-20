354c3b53144f62008c223c0a1c219b8d0d0c209f
Lev Plankis <139138451+levvo1104@users.noreply.github.com>
2025-09-20 16:31:24 -0400

    Basic Game UI Class
    Very far from being done

---
+++ Game.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/* Game User Interface Class
+*
+*  used in Driver to complete
+*  game loop
+*/
+public class Game extends JFrame {
+    private JPanel panel;
+    private JLabel title;
+    private JLabel currentPoints;
+    private int points;
+    private JLabel currentAttempts;
+    private int attempts;
+    private JLabel prompt;
+    private JTextField guess;
+    public Game(int level) {
+        setTitle("Anagrams - Level " +  level);
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel(new FlowLayout());
+        String type = "";
+        switch (level) {  // figure out which cipher
+            case 1: {
+                type = "Caesar";
+                break;
+            }
+            case 2: {
+                type = "Vigenere";
+                break;
+            }
+            case 3: {
+                type = "Aristocrat";
+                break;
+            }
+            case 4: {
+                type = "Rail Fence";
+                break;
+            }
+            default:
+                type = "Baconian";
+        }
+        title = new JLabel(type + " Cipher");
+        currentPoints = new JLabel("Points: " + points);
+        currentAttempts = new JLabel("Attempts: " + attempts);
+        prompt = new JLabel("Guess: ");
+        guess = new JTextField(15);
+
+        // enter button
+        JButton enter = new JButton("Enter");
+        enter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: take in user input and compare to correct answer
+                // TODO: figure out how many points earned if correct
+                attempts++; // add 1 attempt
+            }
+        });
+
+        // initial UI
+        panel.add(title);
+        panel.add(currentPoints);
+        panel.add(currentAttempts);
+        panel.add(prompt);
+        panel.add(guess);
+        panel.add(enter);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) { // for debugging Game UI
+        new Game(3);
+    }
+}
