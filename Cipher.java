7a9d2884ab194018ab35526db1964bc46b2ac3da
pshar1074 <shar1074@purdue.edu>
2025-09-20 14:43:42 -0400

    Cipher.java
    added aristocrats

--- Cipher.java
+++ Cipher.java
@@ -24,10 +24,31 @@ public class Cipher {
         //System.out.println(encrypt);
         return encrypt;
     }
-    
+
+    //untested hashtag i do not want to do this
     public static String Aristocrat (String msg, String keyw, int key) {
         String encrypt = "";
         
+        String aristAlph = keyw;    //alphabet with the key starting it
+        String[] uniqChars = new String[keyw.length()]; //uniq chars in keyword
+        String temp = alph.substring(keyw.length()-1, 26);
+        for (int i = 0; i < uniqChars.length; i++) {
+            if (temp.indexOf(uniqChars[i]) != -1) {
+                temp = temp.replaceAll(uniqChars[i], "");
+            }
+        }
+        
+        aristAlph += temp;  //rest of it
+        String keyedAlph = keyingAlphabet(aristAlph, key);  //getting the keyed alphabet
+        
+        //encrypting the string
+        for (int i = 0; i < msg.length(); i++) {
+            char ch = msg.charAt(i);
+            int index = alph.indexOf(ch);
+            encrypt += keyedAlph.charAt(index);
+        }
+        
+        //System.out.println(encrypt);
         return encrypt;
     }
     
