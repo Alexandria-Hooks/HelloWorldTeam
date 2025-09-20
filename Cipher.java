ef98c435e2d22a79192cdad1b91d9286d54b1e59
Lev Plankis <139138451+levvo1104@users.noreply.github.com>
2025-09-20 18:49:12 -0400

    Update Cipher.java
    fix bugs

--- Cipher.java
+++ Cipher.java
@@ -2,7 +2,7 @@
 public class Cipher {
     public static String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
-    public static String keyingAlphabet (String alph, int key) {
+    public static String keyingAlphabet(String alph, int key) {
         String keyedAlph = "";
         
         keyedAlph = alph.substring(key-1, alph.length());
@@ -11,9 +11,8 @@ public class Cipher {
         return keyedAlph;
     }
     
-    public static String CaesarCipher (String msg, int key) {
+    public static String CaesarCipher(String msg, int key) {
         String encrypt = "";
-        String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String keyedAlph = keyingAlphabet(alph, key);
         
         //encrypting the string
@@ -28,13 +27,13 @@ public class Cipher {
     }
 
     //untested hashtag i do not want to do this
-    public static String Aristocrat (String msg, String keyw, int key) {
+    public static String Aristocrat(String msg, String keyw, int key) {
         String encrypt = ""; 
         String aristAlph = keyw;    //alphabet with the key starting it
         String[] uniqChars = new String[keyw.length()]; //uniq chars in keyword
-        String temp = alph2.substring(keyw.length()-1, 26);
+        String temp = alph.substring(keyw.length()-1, 26);
         for (int i = 0; i < uniqChars.length; i++) {
-            if (temp.indexOf(uniqChars[i]) != -1) {
+            if (temp.contains(uniqChars[i])) {
                 temp = temp.replaceAll(uniqChars[i], "");
             }
         }
@@ -53,16 +52,16 @@ public class Cipher {
         return encrypt;
     }
 
-    public static String XenocryptCipher (String msg, String keyw, int key) {
+    public static String XenocryptCipher(String msg, String keyw, int key) {
         String encrypt = "";
-        //spanish alphabete
+        //spanish alphabet
         String alph2 = "ABCDEFGHIJKLMÑOPQRSTUVWXYZ";
   
         String aristAlph = keyw;    //alphabet with the key starting it
         String[] uniqChars = new String[keyw.length()]; //uniq chars in keyword
         String temp = alph.substring(keyw.length()-1, 26);
         for (int i = 0; i < uniqChars.length; i++) {
-            if (temp.indexOf(uniqChars[i]) != -1) {
+            if (temp.contains(uniqChars[i])) {
                 temp = temp.replaceAll(uniqChars[i], "");
             }
         }
@@ -82,30 +81,34 @@ public class Cipher {
     }
 
 
-     public static String VigenereCipher(String message, String key, boolean encrypt){
-        String Alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder answer = new StringBuilder();
+    public static String VigenereCipher(String message, String key, boolean encrypt){
+        StringBuilder result = new StringBuilder();
 
-        //to match message lenght 
-        StringBuilder ActulKey = new StringBuilder();
+        //to match message length
+        StringBuilder actualKey = new StringBuilder();
+        char a;
         for(int i = 0; i < message.length(); i++){
-            char a = key.charAt (i % key.length());
-            ActaualKey.append(a);
+            a = key.charAt(i % key.length());
+
+            actualKey.append(a);
         }
         //process message (hopefully)
+        char ch;
+        int chIndex;
+        int actualIndex;
+        int result1;
         for (int i = 0; i < message.length(); i++){
-             char ch = message.ChartAt(i);
+            ch = message.charAt(i);
             if(alph.indexOf(ch) != -1){ //encrypt letters
-                int lol = alph.indexOf(ch);
-                int what = alph.indexOf(ActaualKey.charAt(i));
-                int result1;
+                chIndex = alph.indexOf(ch);
+                actualIndex = alph.indexOf(actualKey.charAt(i));
 
                 if(encrypt){
-                    result1 = (lol + what) % 26; //encryption 
+                    result1 = (chIndex + actualIndex) % 26; //encryption
                 } else{
-                    result1 = (lol - waht + 26) % 26; //decryption 
+                    result1 = (chIndex - actualIndex + 26) % 26; //decryption
             }
-                result.append(alph.CharAt(result1));
+                result.append(alph.charAt(result1));
             } else{
                 result.append(ch);
             } 
