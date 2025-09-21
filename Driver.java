9a2393d34c83377cdd0eb45726ea208407473422
Alexandria-Hooks <hooks8@purdue.edu>
2025-09-20 23:52:34 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,7 +7,7 @@ import java.util.Random;
 public class Driver {
     private static String level = "1";
     public static void main(String[] args) {          // initialize quote list
-           // initialize keyword list
+        // initialize keyword list
         // Create UI and obtain user chosen level
         new UI(level1 -> {
             level = level1;
@@ -17,7 +16,7 @@ public class Driver {
             if (!(level.equals("1") || level.equals("2") || level.equals("3") || level.equals("4") || level.equals("5"))) {
                 System.out.println("How are you going to do cryptography if you can't even read?");
             } else { // Game Loop
-                new Game(Integer.parseInt(level)) {
+                new Game(Integer.parseInt(level),questionGenerator()) {
                     // TODO: maybe add stuff later
                 };
             }
@@ -38,32 +37,46 @@ public class Driver {
         }
         return listed;
     }
-    public static ArrayList<String> quotes = lists("YOUR PATH TO QuoteSheet.csv HERE");
+    public static ArrayList<String> quotes = lists("/Users/alexandriahooks/Downloads/QuoteSheet.csv");
     public static int key = keyGenerator();
-    public static ArrayList<String> keywords = lists("YOUR PATH TO Keywords_Sheet1.csv HERE");
+    public static ArrayList<String> keywords = lists("/Users/alexandriahooks/Downloads/Keywords-Sheet1.csv");
     public static String questionGenerator () {
+
         String encrypted = "";
         String type = "";
-        String question = "The encrypted message is " + encrypted + ". It is encoded using a " + type + " cipher.";
         switch (level){
-            case "1": 
+            case "1":
                 type = "caesar";
                 encrypted = Cipher.CaesarCipher(quotes.get(key),key);
-            case "2": 
+                System.out.println(quotes.get(key));
+                break;
+            case "2":
                 type = "vigenere";
-                encrypted = Cipher.VigenereCipher(quotes.get(key),keywords.get(key-4), true);
-                
-                
-                
-                
+                encrypted = Cipher.VigenereCipher(quotes.get(key),keywords.get(key), true);
+                break;
+            case "3":
+                type = "aristocrat";
+                encrypted = Cipher.Aristocrat(quotes.get(key),keywords.get(key),key);
+                break;
+            default:
+                type = "xenocrypt";
+                encrypted = Cipher.XenocryptCipher(quotes.get(key),keywords.get(key), key);
+                break;
+
+
+
+
+
         }
-        
+        String question = "The encrypted message is " + encrypted + ". It is encoded using a " + type + " cipher.";
         return question;
     }
 
     //Random Generator
+
     public static int keyGenerator() {
         Random rand = new Random();
-        return rand.nextInt(10);
+        return rand.nextInt(19);
     }
 }
+
