65796f69553ac53c174108c103ade51e1521bedc
Lev Plankis <139138451+levvo1104@users.noreply.github.com>
2025-09-20 20:37:08 -0400

    Update Cipher.java
    Fixed alphabet shifting and Caesar cipher

--- Cipher.java
+++ Cipher.java
@@ -2,28 +2,29 @@
 public class Cipher {
     public static String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
+    // Shifts alphabet by key to the left
     public static String keyingAlphabet(String alph, int key) {
-        String keyedAlph = "";
-        
-        keyedAlph = alph.substring(key-1, alph.length());
-        keyedAlph += alph.substring(0, key-1);
-        
-        return keyedAlph;
+        return alph.substring(key) + alph.substring(0, key);
     }
     
     public static String CaesarCipher(String msg, int key) {
-        String encrypt = "";
+        StringBuilder encrypt = new StringBuilder();
         String keyedAlph = keyingAlphabet(alph, key);
         
         //encrypting the string
+        char ch;    // current character
+        int index;  // index of current character
         for (int i = 0; i < msg.length(); i++) {
-            char ch = msg.charAt(i);
-            int index = alph.indexOf(ch);
-            encrypt += keyedAlph.charAt(index);
+            ch = msg.charAt(i);
+            if (alph.contains(Character.toString(ch))) {
+                index = alph.indexOf(ch);
+                encrypt.append(keyedAlph.charAt(index));
+            } else {
+                encrypt.append(ch);
+            }
         }
-        
-        //System.out.println(encrypt);
-        return encrypt;
+
+        return encrypt.toString();
     }
 
     //untested hashtag i do not want to do this
