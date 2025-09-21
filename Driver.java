a4ab1091ae4246c0c5f3fbe8609bf5ad2cbfee60
Lev Plankis <139138451+levvo1104@users.noreply.github.com>
2025-09-21 09:10:01 -0400

    Final Driver.java
    Completely functional and no extra lines of commented out code

--- Driver.java
+++ Driver.java
@@ -4,14 +4,12 @@ public class Driver {
         // Create UI and obtain user chosen level
         new UI(level1 -> {
             level = level1;
-
+            
             // check input
             if (!(level.equals("1") || level.equals("2") || level.equals("3") || level.equals("4"))) {
                 System.out.println("How are you going to do cryptography if you can't even read?");
             } else { // Game Loop
-                new Game(Integer.parseInt(level), 0) { // TODO: change to phrase
-
-                };
+                new Game(Integer.parseInt(level), 0);
             }
         });
     }
