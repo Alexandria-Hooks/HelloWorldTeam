2b8dcbd11f020679fae6b1f581c665ecad6cef28
Lev Plankis <139138451+levvo1104@users.noreply.github.com>
2025-09-21 09:06:55 -0400

    Update Game.java
    Functional, however xenocrypt needs help

--- Game.java
+++ Game.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /* Game User Interface Class
 *
@@ -13,7 +17,7 @@ public class Game extends JFrame {
     private JLabel title;
     private String encryption;      // phrase to be solved
     private JLabel currentPoints;
-    private double points = 0.0;    // user's total points
+    private double points;    // user's total points
     private int max_points;         // total possible points from a question
     private JLabel currentAttempts;
     private int attempts = 0;
@@ -21,19 +25,38 @@ public class Game extends JFrame {
     private JLabel question;
     private JLabel prompt;
     private JTextField guess;       // user guess
+    ArrayList<String> quotes;       // phrases
+    ArrayList<String> keywords;
+    String phrase;
 
-    public Game(int level, String phrase) { // Constructor
+    public Game(int level, double inPoints) { // Constructor
+        points = inPoints; // obtain current player points
+        // Initialize quotes and keywords
+        quotes = lists("C:\\Users\\secsa\\OneDrive\\Desktop\\Computer Science\\Purdue\\HelloWorld\\QuoteSheet.csv");          // initialize quote list
+        keywords = lists("C:\\Users\\secsa\\OneDrive\\Desktop\\Computer Science\\Purdue\\HelloWorld\\Keywords_Sheet1.csv");   // initialize keyword list
+
+
+
         setTitle("Anagrams - Level " +  level);
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel = new JPanel(new FlowLayout());
 
+        // initialize parameters
         String type = "";
         max_points = 100; // max possible points
         encryption = "";
-        int key = keyGenerator();
-        String keyw = "DOVE";  // TODO: use real keyw
+        String keyw;
+        if (level == 1) {
+            phrase = quotes.get(randomGenerator(25,21)); // spanish quotes
+            keyw = keywords.get(randomGenerator(25, 21)).toUpperCase();  // random spanish word
+        } else {
+            phrase = quotes.get(randomGenerator(20, 0)).toUpperCase();
+            keyw = keywords.get(randomGenerator(20, 0)).toUpperCase();  // obtain random key word
+        }
+        System.out.println("Encrypted Phrase: " + phrase); // TODO: comment out, for debugging
+        int key = randomGenerator(19, 1);
         String encryptionKey = ""; // stuff to use to solve encryption
         switch (level) {  // figure out which cipher and obtain encryption
             case 1: {
@@ -52,14 +75,14 @@ public class Game extends JFrame {
                 type = "Aristocrat";
                 max_points = 500;
                 encryption = Cipher.Aristocrat(phrase, keyw, key);
-                encryptionKey = "word '" + keyw + "' and key " + Integer.toString(key);
+                encryptionKey = "word '" + keyw + "' and key " + key;
                 break;
             }
             default: {
                 type = "Xenocrypt";
                 max_points = 1000;
-                encryption = Cipher.XenocryptCipher(phrase, "DOVE", key);
-                encryptionKey = "word '" + keyw + "' and key " + Integer.toString(key);
+                encryption = Cipher.XenocryptCipher(phrase, keyw, key);
+                encryptionKey = "word '" + keyw + "' and key " + key;
             }
         }
         title = new JLabel(type + " Cipher");
@@ -103,7 +126,7 @@ public class Game extends JFrame {
                         int choice = JOptionPane.showConfirmDialog(null, "Would you like to move to the next level?");
                         if (choice == JOptionPane.YES_OPTION) {
                             dispose();
-                            new Game(level + 1, "NEW PHRASE HERE"); // TODO: get new phrase
+                            new Game(level + 1, points);
                         }
                         dispose(); // close UI
                     } else {
@@ -112,7 +135,7 @@ public class Game extends JFrame {
                         next.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
-                                new Game(level, "NEW PHRASE HERE"); // TODO: get a new phrase
+                                new Game(level, points);
                             }
                         });
                         panel.add(next);
@@ -147,9 +170,9 @@ public class Game extends JFrame {
         setVisible(true);
     }
 
-    // Random Key Generator (1 to 19)
-    public static int keyGenerator() {
-        return (int) (Math.random() * (18) + 1);
+    // Random Number Generator for min to max
+    public static int randomGenerator(int max, int min) {
+        return (int) (Math.random() * (max - min) + min);
     }
 
     // Calculate points gained based on number of attempts
@@ -158,10 +181,28 @@ public class Game extends JFrame {
             return inPoint;
         return inPoint * Math.pow(0.9, (inAttempts - 1));
     }
-/*
+
+    // String lists - takes csv path & creates list
+    public static ArrayList<String> lists(String path){
+        String line;
+        ArrayList<String> listed = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null){
+                listed.add(line);
+            }
+
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return listed;
+    }
+
     public static void main(String[] args) { // for debugging Game UI
-        new Game(3, "DOVE IS BIRD");
+        new Game(4, 0);
         //System.out.println(keyGenerator());
         //System.out.println(getScore(100, 2));
-    }*/
+    }
 }
