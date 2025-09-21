594c26519be0a11da68a4f964ebe04ee57be2413
annetti-25 <annetti.belden@gmail.com>
2025-09-21 10:10:35 -0400

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -69,7 +69,7 @@ public class Cipher {
 
     public static String XenocryptCipher(String msg, String keyw, int key) { // TODO: FIX
         StringBuilder encrypt = new StringBuilder();
-        static String spanishAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static String spanishAlph = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
         String aristAlph = keyw;    // alphabet with the key starting it
         String[] uniqChars = new String[keyw.length()]; //uniq chars in keyword
